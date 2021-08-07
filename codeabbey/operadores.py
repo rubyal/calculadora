6ec3e552045c2d151f8b1858378bf21598e1efe5
rubyal <rubitaalvarez99@gmail.com>
2021-08-06 21:29:09 -0500

    se añade ciclo for

--- codeabbey/operadores.py
+++ codeabbey/operadores.py
@@ -1,8 +1,4 @@
 #numeros=[1,2,3,4,5,6,7,8,9,10]
 print("se inicia programa")
-numero=1
-hasta=int(input())
-while(numero<=hasta):
+for numero in range(10,20):
     print(numero)
-    numero=numero+1
-
