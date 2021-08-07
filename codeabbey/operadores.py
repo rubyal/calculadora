521e86dea2c4cb67c08445e4b2fbb8977883a992
rubyal <rubitaalvarez99@gmail.com>
2021-08-06 21:51:23 -0500

    se añade lista de conversion

--- codeabbey/operadores.py
+++ codeabbey/operadores.py
@@ -1,9 +1,5 @@
-numeros=[1,2,3,4,5,6,7,8,9,10]
-print(numeros)
-dato=input()
-numero=int(dato)
-if(numero in numeros):
-    print("SI")
-if(numero not in numeros):
-    print("NO")
+print("SE INICIA PROGRAMA")
+entrada=input().split(" ")
+entrada2=list(map(int,entrada))
+print(entrada2)
 
