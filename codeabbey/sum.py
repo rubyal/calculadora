85145e99768b3cc6e5e01a37ed8ba0949581fca0
rubyal <rubitaalvarez99@gmail.com>
2021-08-06 19:43:50 -0500

    función agregar numeros

---
+++ codeabbey/sum.py
@@ -0,0 +1,10 @@
+print("Introduce el primer numero")
+dato1=int(input())
+print(dato1)
+print("Introduce el segundo numero")
+dato2=int(input())
+print(dato2)
+suma=dato1+dato2
+suma2=str(suma)
+resultado="la suma es " + suma2
+print(resultado)
