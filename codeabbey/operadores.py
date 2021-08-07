071237cf267aa0986ccb617bb2bac24a3fa1c3d9
rubyal <rubitaalvarez99@gmail.com>
2021-08-06 21:08:16 -0500

    si se añade condicionales aninados

--- codeabbey/operadores.py
+++ codeabbey/operadores.py
@@ -1,10 +1,12 @@
-nota1=6
-nota2=4
-nota3=7
-suma=nota1+nota2+nota3
-division=suma/3
-print(division)
-nota_media=division
-if(division>=5):
-    print("aprobado")
+print("inserte valor")
+minimo=20
+maximo=500
+dato=int(input())
+numero=dato
+if(numero<minimo):
+    print("valor bajo")
+if(numero>maximo):
+    print("valor alto")
+if(numero>minimo) and (numero <maximo):
+    print("valor medio")
 
