a1db7f1c9e5bca1a469fab3d8ecfceba1109602f
rubyal <rubitaalvarez99@gmail.com>
2021-08-09 17:58:43 -0500

    se añade condicionales IF, Else, Elif

--- codeabbey/SuminLoop.py
+++ codeabbey/SuminLoop.py
@@ -1,8 +1,41 @@
 print("SE INICIA PROGRAMA")
-diccionario={"uno":"one","dos":"two","tres":"three"}
-print(diccionario)
-diccionario["cuatro"]="four"
-print(diccionario)
-dato=input()
-valor=diccionario[dato]
-print(valor)
+a=8
+b=4
+c=2
+d=6
+print(a>b)
+print(a<b)
+if a > b:
+    print("a es mayor que b")
+if a < b:
+    print("a es menor que b")
+if (a<b) and (b<d):
+    print("la primera es correcta")
+else:
+    print("la primera no es correcta")
+print("OTRA")
+if a>b:
+    print("a es mayor que b")
+elif (a==b):
+    print("a es igual a b")
+else:
+    print("ninguna es correcta")
+print("OTRA")
+a=8
+b=8
+if a>b:
+    print("a es mayor que b")
+elif (a==b):
+    print("a es igual a b")
+else:
+    print("ninguna es correcta")
+print("OTRA")
+a=8
+b=10
+if a>b:
+    print("a es mayor que b")
+elif (a==b):
+    print("a es igual a b")
+else:
+    print("ninguna es correcta")
+
