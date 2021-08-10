01525c1447685b10cbac0386ca5978af57f0f5b3
rubyal <rubitaalvarez99@gmail.com>
2021-08-09 19:06:28 -0500

    Se añade while continue

--- codeabbey/SuminLoop.py
+++ codeabbey/SuminLoop.py
@@ -2,10 +2,11 @@ print("SE INICIA PROGRAMA")
 valor=1
 fin=10
 while(valor<fin):
-    print(valor)
     valor=valor+1
-    if (valor==5):
-        break
+    if (valor==6):
+        continue
+    print(valor)
+
 
 
 
