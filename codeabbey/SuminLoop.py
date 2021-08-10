ff627197fbdeb7b49119a8083981522b4392d565
rubyal <rubitaalvarez99@gmail.com>
2021-08-09 19:25:08 -0500

    se añade ejercicio 1 condicional diccionario

--- codeabbey/SuminLoop.py
+++ codeabbey/SuminLoop.py
@@ -1,13 +1,8 @@
 print("SE INICIA PROGRAMA")
-valor=1
-fin=10
-while(valor<fin):
-    valor=valor+1
-    if (valor==6):
-        continue
-    print(valor)
-
-
-
-
-
+frutas={"manzana":"apple","naranja":"orange","platano":"banana","limon":"lemon"}
+print(frutas)
+print("naranja")
+frutas["piña"]="pimeapple"
+print(frutas)
+for clave,valor in frutas.items():
+    print("{} en ingles es {}".format(clave,valor))
