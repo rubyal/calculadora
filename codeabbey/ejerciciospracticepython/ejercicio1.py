128ad5232350cf87a764659f1347578499ec9477
rubyal <rubitaalvarez99@gmail.com>
2021-08-10 13:41:41 -0500

    ejercicio1 charter input

---
+++ codeabbey/ejerciciospracticepython/ejercicio1.py
@@ -0,0 +1,7 @@
+#ingresar nombre y edad
+print("SE INICIA PROGRAMA")
+print("ingrese su nombre")
+nombre=str(input())
+print("ingrese su edad")
+edad=int(input())
+print("su nombre es {} y tiene {}".format(nombre,edad))
